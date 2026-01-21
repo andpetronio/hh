babbdc5e73ccc3af14a2950eaee74fffb5d2c7e2
petro232 <andpetronio@gmail.com>
2026-01-21 15:11:43 -0300

    fix: static astro deploy on vercel

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,9 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
-import vercel from "@astrojs/vercel";
 
-// https://astro.build/config
 export default defineConfig({
   integrations: [tailwind()],
   output: 'static',
-  adapter: vercel(),
   i18n: {
     defaultLocale: 'es',
     locales: ['es', 'pt-BR'],
